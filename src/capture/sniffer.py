233558f55ca528aeb8c84e56371ef53dd9bafa7b
Aziz Ben Hammouda <azizbhammouda@gmail.com>
2026-01-02 22:13:47 +0100

    writing sniffer code

---
+++ src/capture/sniffer.py
@@ -0,0 +1,36 @@
+from scapy.all import sniff,IP,TCP,UDP
+def organize_packet(packet):
+  if IP not in packet:
+    return None
+  packet_data={
+    "src_ip":packet[IP].src,
+    "dest_ip":packet[IP].dst,
+    "protocol":None,
+    "port_src":None,
+    "port_dst":None
+  }
+  if TCP in packet:
+    packet_data["protocol"]="TCP"
+    packet_data["port_src"]=packet[TCP].sport
+    packet_data["port_dst"]=packet[TCP].dport
+  elif UDP in packet:
+    packet_data["protocol"]="UDP"
+    packet_data["port_src"]=packet[UDP].sport
+    packet_data["port_dst"]=packet[UDP].dport
+  else:
+     packet_data["protocol"]="OTHER"
+
+  return packet_data
+  
+def packet_exec(packet):
+    organized=organize_packet(packet)
+    if organized:
+      print (organized)
+def start_sniffing(interface=None):
+    sniff(iface=interface, prn=packet_exec, store=False)
+if __name__ == "__main__":
+    print("Starting packet capture...")
+    start_sniffing()
+
+ 
+
