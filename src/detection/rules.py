b3a6502cc26009afb7309760e56687cc7f0ace56
Aziz Ben Hammouda <azizbhammouda@gmail.com>
2026-01-04 14:27:50 +0100

    fixed SSH rule

--- src/detection/rules.py
+++ src/detection/rules.py
@@ -1,6 +1,6 @@
 def packet_receiver(packet):
-  received=organize_packet(packet)
-  if received:
-    #possible SSH
-    if received["port_dst"]==22:
-      return "[ALERT] Possible SSH access from"+received["src_ip"]+"->"+ received["dest_ip"]
+      #possible SSH
+      if packet["dst_port"]==22:
+            return "[ALERT] Possible SSH access from"+received["src_ip"]+"->"+ received["dest_ip"]
+  
+      
