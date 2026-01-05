740add297be24e2a5b9722d6bc7d2be8478a1731
Aziz Ben Hammouda <azizbhammouda@gmail.com>
2026-01-05 16:14:41 +0100

    added connection tracker for brute force

--- src/detection/rules.py
+++ src/detection/rules.py
@@ -1,4 +1,17 @@
-def packet_receiver(packet):
+from collections import defaultdict
+from datetime import datetime, timedelta
+
+
+connection_tracker=defaultdict(lambda:{"count":0,"last seen":None,"port":set()})
+failed_login_tracker=defaultdict(int)
+
+def track_connections(src_ip,port):
+      now=datetime.now()
+      tracker=connection_tracker[src_ip]
+
+
+def ssh_brute_force(packet):
+      alerts=[]
       #possible SSH
       if packet["dst_port"]==22:
             return "[ALERT] Possible SSH access from"+received["src_ip"]+"->"+ received["dest_ip"]
